35760c476d871946dced05f9cddeaaafc9fbc1a0
Xiang Zhou <xiang_zhou@fas.harvard.edu>
2016-11-21 21:33:56 -0500

    add some @importFrom

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,14 @@
 #' @useDynLib strat
 #' @importFrom Rcpp evalCpp
+#' @importFrom Hmisc wtd.table
+#' @importFrom stats approx complete.cases
 NULL
+
 
 wtd_rank <- function (x, weights = NULL, normwt = FALSE, na.rm = TRUE){
   if (!length(weights))
     return(rank(x, na.last = if (na.rm) NA else TRUE))
-  tab <- Hmisc::wtd.table(x, weights, normwt = normwt, na.rm = na.rm)
+  tab <- wtd.table(x, weights, normwt = normwt, na.rm = na.rm)
   freqs <- tab$sum.of.weights
   r <- cumsum(freqs) - 0.5 * (freqs - 1)
   stats::approx(tab$x, r, xout = x, rule = 2)$y
